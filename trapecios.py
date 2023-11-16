95cc796240629477a673abbffa02b7c96f5efa32
Andre-Leandro <andreleandrosanlorenzo@gmail.com>
2023-11-16 14:35:12 -0300

    showing error by console

--- trapecios.py
+++ trapecios.py
@@ -1,30 +1,29 @@
 from sympy import symbols, diff
 
-def reglaTrapecio(a, b, funcion, n):
+def reglaTrapecio(a, b, f, n):
+    x = symbols('x')
     h = (b - a) / n
-    resultado = 0.5 * (funcion(a) + funcion(b))
+    resultado = 0.5 * (f(a) + f(b))
 
     for i in range(1, n):
-        resultado += funcion(a + i * h)
+        resultado += f(a + i * h)
     resultado *= h
 
+    segundaDerivada = diff(f(x), x, 2)
 
+    puntoEvaluacion = (a + b) / 2
+    valor_segunda_derivada = segundaDerivada.subs(x, puntoEvaluacion)
 
-    segundaDerivada = diff(f, 2)
+    error = -((b - a) * h**2 / 12) * valor_segunda_derivada
 
-    valor_segunda_derivada = segundaDerivada.subs(((b-a)/2))
-
-    error = -((b-a) * h**2 / 12) * (f(b) - 2 * f(a) + f(a + h))
-
-
-    return resultado
+    return resultado, error 
 
 def f(x):
     return -x**2 + 6*x - 4
 
 a = 1
 b = 4
-n = 6  
+n = 3
 
-aproximacionIntegral = reglaTrapecio(a, b, f, n)
-print("Aproximación de la integral:", aproximacionIntegral)
+aproximacionIntegral, e = reglaTrapecio(a, b, f, n)
+print("Aproximación de la integral:", aproximacionIntegral, "con un error de: ", e)
